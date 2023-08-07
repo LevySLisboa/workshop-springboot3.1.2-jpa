b539cf63beda6b6a5f84717624191b5cd66cac8c
Levy Lisboa <lisboalevy@gmail.com>
2023-08-07 18:20:03 -0300

    OrderStatus enum

--- src/main/java/com/couse/projetoSpring/entities/Order.java
+++ src/main/java/com/couse/projetoSpring/entities/Order.java
@@ -1,11 +1,12 @@
 package com.couse.projetoSpring.entities;
 
+import com.couse.projetoSpring.entities.Enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -16,14 +17,17 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -45,6 +49,16 @@ public class Order implements Serializable {
 
     public User getClient() {
         return client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus!=null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public void setClient(User client) {
